fe2149375c50afa803335aad9d06982cefbfabb5
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-21 22:29:59 -0300

    Authenticate users with username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
+
+  validates :username, uniqueness:true
 
   has_many :notes
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end

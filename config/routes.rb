4b02f742fda201d6d9fa229afe22fa2ed3cda6fe
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-23 16:23:27 -0300

    Avoid redirecting on sign up submission failure
    Since the form was submitted through a POST request to /users this url would end
    up in the browser, leading to a 'no /users route' error if the page was
    refreshed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :notes
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+
+  devise_scope :user do
+    get "user/sign_up", to: "users/registrations#new", as: :new_user_registration
+    post "user/sign_up", to: "users/registrations#create", as: :user_registration
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
